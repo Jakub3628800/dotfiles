80bdf19249b23b0827f6efcb97515e6d35d0f981
Jakub3628800 <JakubyKriz@gmail.com>
2020-11-12 22:57:40 +0100

    add fish_variables

--- create_links.sh
+++ create_links.sh
@@ -4,3 +4,4 @@ ln -f ~/.sound.sh .sound.sh
 ln -f ~/.bash_aliases .bash_aliases
 ln -f ~/.bashrc .bashrc
 ln -f ~/.xmonad/xmonad.hs ./.xmonad/xmonad.hs
+ln -f ~/.config/fish/fish_variables ./.config/fish/fish_variables
